ae94e6710e1fe77b61c1d52e3cae9779b256d7bc
simw <simw@users.noreply.github.com>
2014-11-24 17:45:23 -0800

    Updated readme and setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
-from distutils.core import setup
+
+from distutils.core import setup, find_packages
+from io import open
 
 setup(
     name='Strex',
     version='0.1.0',
+    
     author='simw',
     author_email='jrh@example.com',
-    packages=['strex', 'tests'],
+    
+    packages=find_packages(exclude=['tests*']),
+    
     url='http://github.com/simw/strex',
     license='LICENSE.txt',
-    description='HTML / XML to object structure extractor',
+    
+    description='Extracts structure from unstructured data',
     long_description=open('README.md').read(),
+    
     install_requires=[
-        "lxml >= 3.3.0",
+        'lxml >= 3.3.0',
+        'requests >= 2.4.0',
     ],
+
+    extras_require = {
+        'test': ['pytest'],
+    },
 )
+
